9d50c822c64d71800e384be250ab7e9bb84530bd
antonsuba <antonsuba@outlook.com>
2017-11-10 12:43:40 +0800

    Fully implement back propagation and runner

--- neural_network/neural_network.py
+++ neural_network/neural_network.py
@@ -12,21 +12,13 @@ class NeuralNetwork(object):
         self.weights = self.__setup_weights(topology, weight)
         self.output = output
         self.activation_scheme = activation_scheme
+
         self.guess_list = list()
         self.error_list = list()
         self.layers_with_weights = list()
 
-        def __relu_activation(x):
-            return x if x > 0 else 0
-
-
-        def __sigmoid_activation(x):
-            return 1 / (1 + math.exp(-1 * x))
-
-        self.activation_functions = {
-            'Relu' : __relu_activation,
-            'Sigmoid' : __sigmoid_activation
-        }
+        self.activation_functions = self.__setup_activation_functions()
+        self.derivative_functions = self.__setup_derivative_functions()
 
 
     def __setup_layers(self, topology, inputs):
@@ -46,6 +38,46 @@ class NeuralNetwork(object):
             weights.append(weights_matrix)
 
         return weights
+
+
+    def __setup_activation_functions(self):
+        def __relu_activation(x):
+            return x if x > 0 else 0
+
+        def __sigmoid_activation(x):
+            return 1 / (1 + math.exp(-1 * x))
+
+        activation_functions = {
+            'Relu' : __relu_activation,
+            'Sigmoid' : __sigmoid_activation
+        }
+
+        return activation_functions
+
+
+    def __setup_derivative_functions(self):
+        def __relu_derivative(x):
+            return 1 if x > 0 else 0
+
+        def __sigmoid_derivative(x):
+            return x * (1 - x)
+
+        derivative_functions = {
+            'Relu' : __relu_derivative,
+            'Sigmoid' : __sigmoid_derivative
+        }
+
+        return derivative_functions
+
+
+    def __generate_current_topology(self):
+        "Generate list of layers and weights"
+
+        layers_with_weights = [None] * (len(self.layers) + len(self.weights))
+        layers_with_weights[::2] = self.layers
+        layers_with_weights[1::2] = self.weights
+
+        self.layers_with_weights = layers_with_weights
 
 
     def feed_forward(self):
@@ -70,30 +102,23 @@ class NeuralNetwork(object):
         error_rate = error_rate_func(self.guess_list[-1], self.output)
         self.error_list.append(error_rate)
 
-        #Generate list of layers and weights
-        layers_with_weights = [None] * (len(self.layers) + len(self.weights))
-        layers_with_weights[::2] = self.layers
-        layers_with_weights[1::2] = self.weights
-
-        self.layers_with_weights.append(layers_with_weights)
-
-        return layers_with_weights
+        self.__generate_current_topology()
 
 
     def back_propagation(self):
-
-        def __compute_y_derivative(x):
-            return (1 - x) * x
 
         def __compute_gradient(y, error):
             return y * error
 
         guess = self.guess_list[-1]
         error_rate = self.error_list[-1]
+
+        #First part of back propagation
         prev_layer = self.layers[-2]
         prev_weight = self.weights[-1]
 
-        y_derivative_func = np.vectorize(__compute_y_derivative)
+        key = self.activation_scheme[-1]
+        y_derivative_func = np.vectorize(self.derivative_functions[key])
         y_derivative = y_derivative_func(guess)
 
         gradient_func = np.vectorize(__compute_gradient)
@@ -108,12 +133,42 @@ class NeuralNetwork(object):
         self.weights[-1] = new_weight
 
 
-topology = [4, 2, 3]
-inputs = [1, 0, 1, 0]
-output = [1, 0, 0]
-activation = ['Relu', 'Sigmoid']
-neural_net = NeuralNetwork(topology, inputs, output, activation, 0.2)
-topo = neural_net.feed_forward()
-neural_net.back_propagation()
-# print(str(topo))
-print(str(neural_net.weights))
+        #Second Part of back propagation
+        gradients_p = gradients
+        weights_p = prev_weight
+
+        for i in range(2, len(self.weights) + 1):
+            layer = self.layers[-i]
+            layer_next = self.layers[-(i+1)]
+
+            key = self.activation_scheme[-i]
+            derivative_function = np.vectorize(self.derivative_functions[key])
+            z_hat = derivative_function(layer)
+
+            weights_p_tr = weights_p.transpose()
+            gradients_h = gradients_p * weights_p_tr
+            gradients_h_activated = gradient_func(gradients_h, z_hat)
+
+            layer_next_tr = layer_next.transpose()
+            delta_w = layer_next_tr * gradients_h_activated
+
+            original_weight = self.weights[-i]
+            new_weight = original_weight - delta_w
+
+            gradients_p = gradients_h_activated
+            weights_p = original_weight
+
+            self.weights[-i] = new_weight
+
+        self.__generate_current_topology()
+
+
+    def train(self, epochs):
+        for i in range(0, epochs):
+            print('Feed forward %i' % i)
+            self.feed_forward()
+            print(str(self.layers_with_weights))
+
+            print('Back propagation %i' % i)
+            self.back_propagation()
+            print(str(self.layers_with_weights))            
